8297777497e13b196b7f276eaa1e5abe5594c228
arx <crystalsoul2k@gmail.com>
2026-09-10 23:29:08 +0700

    Add XDG portal and default directory application

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -6,6 +6,11 @@
   services.udisks2.enable = true;
   services.gvfs.enable = true;
 
+  xdg.portal.enable = true;
+  xdg.mime.defaultApplications = {
+    "inode/directory" = "org.kde.dolphin.desktop";
+  };
+
   environment.etc."xdg/menus/applications.menu".source =
     "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
 }
--- modules/packages.nix
+++ modules/packages.nix
@@ -16,5 +16,6 @@
     tree
     usbutils
     pciutils
+    xdg-utils
   ];
 }
