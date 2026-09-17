24927fb3f57e2029d8b45a4bab51b356b4528959
arx <crystalsoul2k@gmail.com>
2026-09-17 17:12:43 +0700

    feat: add mpv scripts

--- home/mpv.nix
+++ home/mpv.nix
@@ -1,6 +1,11 @@
+{ pkgs, ... }:
+
 {
   programs.mpv = {
     enable = true;
+    scripts = with pkgs.mpvScripts; [
+      mpris
+    ];
     config = {
       keep-open = "yes";
       loop-playlist = "yes";
