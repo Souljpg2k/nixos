ecd96696cef4fd596a5c8cfffa8947a7bbe7f752
arx <crystalsoul2k@gmail.com>
2026-09-25 22:35:40 +0700

    c

--- home.nix
+++ home.nix
@@ -5,8 +5,10 @@
   home.homeDirectory = "/home/arx";
   home.stateVersion = "26.05";
 
-  imports = lib.filesystem.listFilesRecursive ./modules/config 
-    ++ lib.filesystem.listFilesRecursive ./modules/pkgs;
+  imports = lib.filter (lib.hasSuffix ".nix") (
+    lib.filesystem.listFilesRecursive ./modules/config
+    ++ lib.filesystem.listFilesRecursive ./modules/pkgs
+  );
   
   programs.home-manager.enable = true;
 }
