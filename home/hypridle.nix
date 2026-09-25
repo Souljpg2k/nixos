4fc9fe8410b876d8ec40850b70654b696e548494
arx <crystalsoul2k@gmail.com>
2026-09-25 16:34:26 +0700

    use  caelestia:lock

--- home/hypridle.nix
+++ home/hypridle.nix
@@ -4,7 +4,7 @@
 
     settings = {
       general = {
-        lock_cmd = ''hyprctl dispatch 'hl.dsp.global("quickshell:lock")' '';
+        lock_cmd = ''hyprctl dispatch 'hl.dsp.global("caelestia:lock")' '';
         before_sleep_cmd = "loginctl lock-session";
         after_sleep_cmd = ''hyprctl dispatch 'hl.dsp.dpms({ action = "enable" })' '';
       };
