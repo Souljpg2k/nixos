522140bac922626b163bb07ed5d0d6bbca08d018
arx <crystalsoul2k@gmail.com>
2026-09-26 19:48:44 +0700

    replace starship with custom fish_prompt

--- modules/config/fish.nix
+++ modules/config/fish.nix
@@ -1,10 +1,22 @@
 {
   programs.fish = {
     enable = true;
+
     interactiveShellInit = ''
       set fish_greeting
-      starship init fish | source
     '';
+
+    functions = {
+      fish_prompt = ''
+        set_color purple
+        echo -n '['
+        echo -n (prompt_pwd)
+        echo -n ']'
+        echo -n '$ '
+        set_color normal
+      '';
+    };
+
     shellAbbrs = {
       re = "sudo nixos-rebuild switch --flake ~/nixos#nixos";
       cg = "sudo nix-collect-garbage -d";
